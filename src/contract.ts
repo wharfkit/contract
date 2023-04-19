e934edb253c437e7b9b5790f57f6408e75d62d85
dafuga <danielfugere28@gmail.com>
2023-04-18 21:54:01 -0400

    revert: removed redundant code that is in session kit

--- src/contract.ts
+++ src/contract.ts
@@ -4,27 +4,10 @@ import {
     AnyAction,
     ABISerializableObject,
     Action,
-    APIClient,
     Checksum256,
     Name,
     NameType,
-    FetchProvider,
-    ABI,
-    Serializer
 } from '@wharfkit/session'
-
-// stubs for session kit
-interface SessionTransactArgs {
-    actions: AnyAction[]
-}
-interface SessionTransactResult {
-    id: Checksum256
-}
-
-// TODO: move this to core
-export function isABISerializableObject(value: any): value is ABISerializableObject {
-    return value.constructor && typeof value.constructor.abiName === 'string'
-}
 
 export class Contract {
     /** Account where contract is deployed. */
@@ -67,55 +50,18 @@ export class Contract {
     ): Promise<TransactResult> {
         let action: Action
 
-        // Add a RPC provider URL here
-        const apiClient = new APIClient({provider: new FetchProvider(session.chain.url)})
-
-        let serializedData: ABISerializableObject
-
-        if (isABISerializableObject(data)) {
-            serializedData = data
-        } else {
-            /// Fetch the ABI and construct the action
-            const abi = await this._getAbi(apiClient)
-            serializedData = await this._serializeActionData(abi, name, data)
-        }
-
         action = Action.from({
             account: this.account,
             name,
             authorization: [],
-            data: serializedData,
+            data,
         })
 
         try {
             // Trigger the transaction using the session kit
-            return session.transact({actions: [action]})
+            return session.transact({action})
         } catch (error) {
             throw error
         }
-    }
-
-    private async _getAbi(apiClient): Promise<ABI> {
-        const cachedAbi = this.constructor['cachedAbi']
-        if (cachedAbi) {
-            return cachedAbi
-        } else {
-            const abiResult = await apiClient.v1.chain.get_abi(this.account)
-            this.constructor['cachedAbi'] = abiResult.abi
-            return abiResult.abi
-        }
-    }
-
-    private async _serializeActionData(
-        abi: ABI,
-        name: NameType,
-        data: {[key: string]: any}
-    ): Promise<ABISerializableObject> {
-        const actionType = abi.actions.find((a) => a.name === name)?.type
-        if (!actionType) {
-            throw new Error(`Action '${name}' not found in ABI`)
-        }
-
-        return Serializer.encode({object: data, type: actionType})
     }
 }

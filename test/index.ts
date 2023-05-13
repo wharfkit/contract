ea4fdcd22cf50ba64739c4f6c5c77b1733ecc084
dafuga <danielfugere28@gmail.com>
2023-05-12 20:21:02 -0400

    cleanup: removed failing placeholder test

--- test/index.ts
+++ test/index.ts
@@ -2,8 +2,4 @@ import {assert} from 'chai'
 
 import * as lib from '$lib'
 
-suite('index', function () {
-    test('maths', function () {
-        assert.equal(lib.maths(1, 2), 3)
-    })
-})
+suite('index', function () {})

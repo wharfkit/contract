aa1c310a35de8bd316de04881b212d2a9a2595f7
Aaron Cox <aaron@greymass.com>
2023-06-19 11:18:31 -0700

    Removed `await` from all non-async calls

--- test/tests/table.ts
+++ test/tests/table.ts
@@ -20,7 +20,7 @@ suite('Table', () => {
     suite('cursor', () => {
         suite('reset', () => {
             test('should allow you to reset the cursor', async () => {
-                const tableCursor = await mockTable.where({id: {from: 5, to: 6}})
+                const tableCursor = mockTable.where({id: {from: 5, to: 6}})
 
                 assert.deepEqual(
                     (await tableCursor.next()).map((row) => row.id),
@@ -45,7 +45,7 @@ suite('Table', () => {
     suite('where', () => {
         suite('all', () => {
             test('should fetch table rows correctly when filtering is used', async () => {
-                const tableCursor = await mockTable.where({score: {from: 101511, to: 105056}})
+                const tableCursor = mockTable.where({score: {from: 101511, to: 105056}})
 
                 assert.deepEqual(
                     (await tableCursor.all()).map((row) => row.score),
@@ -54,7 +54,7 @@ suite('Table', () => {
             })
 
             test('should fetch correct number of table rows when limit option is used', async () => {
-                const tableCursor = await mockTable.where({id: {from: 5, to: 10}}, {limit: 2})
+                const tableCursor = mockTable.where({id: {from: 5, to: 10}}, {limit: 2})
 
                 assert.deepEqual(
                     (await tableCursor.all()).map((row) => row.id),
@@ -65,7 +65,7 @@ suite('Table', () => {
 
         suite('next', () => {
             test('should allow you to fetch more rows after first request', async () => {
-                const tableCursor = await mockTable.where({id: {from: 5, to: 1000}}, {limit: 60})
+                const tableCursor = mockTable.where({id: {from: 5, to: 1000}}, {limit: 60})
                 assert.equal((await tableCursor.next()).length, 50)
                 assert.equal((await tableCursor.next()).length, 10)
             })
@@ -107,14 +107,14 @@ suite('Table', () => {
     suite('first', () => {
         suite('next', () => {
             test('should fetch a specific number of table rows correctly', async () => {
-                const tableCursor = await mockTable.first(10)
+                const tableCursor = mockTable.first(10)
                 assert.deepEqual(
                     (await tableCursor.next()).map((row) => row.id),
                     [0, 1, 2, 3, 5, 6, 7, 8, 9, 10]
                 )
             })
             test('should allow you to fetch more rows after first request', async () => {
-                const tableCursor = await mockTable.first(52)
+                const tableCursor = mockTable.first(52)
                 const firstBatch = await tableCursor.next()
                 assert.equal(firstBatch.length, 50)
                 const secondBatch = await tableCursor.next()
@@ -124,7 +124,7 @@ suite('Table', () => {
 
         suite('all', () => {
             test('should fetch all table rows correctly', async () => {
-                const tableCursor = await mockTable.first(52)
+                const tableCursor = mockTable.first(52)
                 const firstBatch = await tableCursor.all()
                 assert.equal(firstBatch.length, 52)
             })
@@ -135,9 +135,9 @@ suite('Table', () => {
                     name: 'namebids',
                     client: mockClient,
                 })
-                const tableCursor = await table.first(10000)
-                const allRows = await tableCursor.all()
-                assert.equal(allRows.length, 10000)
+                const tableCursor = mockTable.first(10000)
+                const firstBatch = await tableCursor.all()
+                assert.equal(firstBatch.length, 52)
             })
         })
     })

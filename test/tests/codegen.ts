7ea64c69cccebc46516d558f8c0838e88cf42f5a
Aaron Cox <aaron@greymass.com>
2023-08-20 10:43:15 -0700

    removed try/catch

--- test/tests/codegen.ts
+++ test/tests/codegen.ts
@@ -34,64 +34,60 @@ suite('codegen', async function () {
         test('Contracts are identical', function () {
             assert.equal(files.generated, files.mock)
         })
-        try {
-            for (const contractKey of Object.keys(contracts)) {
-                test(`Testing namespace ${contractKey}`, function () {
-                    // The `RewardsGm` namespace
-                    const testNamespace = contracts[contractKey].RewardsGm
+        for (const contractKey of Object.keys(contracts)) {
+            test(`Testing namespace ${contractKey}`, function () {
+                // The `RewardsGm` namespace
+                const testNamespace = contracts[contractKey].RewardsGm
 
-                    // The `Contract` instance in the namespace
-                    const testContract = testNamespace.Contract
-                    const testContractInstance = new testContract({client})
+                // The `Contract` instance in the namespace
+                const testContract = testNamespace.Contract
+                const testContractInstance = new testContract({client})
 
-                    // Run generic contract tests
-                    runGenericContractTests(testContractInstance)
+                // Run generic contract tests
+                runGenericContractTests(testContractInstance)
 
-                    function assertRewardsContract(contract) {
-                        assert.instanceOf(contract, Contract)
-                        assert.instanceOf(contract.abi, ABI)
-                        assert.isTrue(contract.abi.equals(testNamespace.abi))
-                        assert.instanceOf(contract.account, Name)
-                        assert.instanceOf(contract.client, APIClient)
-                    }
+                function assertRewardsContract(contract) {
+                    assert.instanceOf(contract, Contract)
+                    assert.instanceOf(contract.abi, ABI)
+                    assert.isTrue(contract.abi.equals(testNamespace.abi))
+                    assert.instanceOf(contract.account, Name)
+                    assert.instanceOf(contract.client, APIClient)
+                }
 
-                    assertRewardsContract(testContractInstance)
+                assertRewardsContract(testContractInstance)
 
-                    const c1 = new testContract({client})
-                    assertRewardsContract(c1)
+                const c1 = new testContract({client})
+                assertRewardsContract(c1)
 
-                    const c2 = new testContract({client, abi: testNamespace.abi})
-                    assertRewardsContract(c2)
+                const c2 = new testContract({client, abi: testNamespace.abi})
+                assertRewardsContract(c2)
 
-                    const c3 = new testContract({client, account: 'teamgreymass'})
-                    assertRewardsContract(c3)
+                const c3 = new testContract({client, account: 'teamgreymass'})
+                assertRewardsContract(c3)
 
-                    const c4 = new testContract({
-                        client,
-                        account: Name.from('teamgreymass'),
-                    })
-                    assertRewardsContract(c4)
-
-                    const c5 = new testContract({
-                        client,
-                        abi: testNamespace.abi,
-                        account: 'teamgreymass',
-                    })
-                    assertRewardsContract(c5)
-
-                    assert.throws(() => new testContract({abi: testNamespace.abi}))
-                    assert.throws(
-                        () =>
-                            new testContract({
-                                abi: testNamespace.abi,
-                                account: 'teamgreymass',
-                            })
-                    )
-                    assert.throws(() => new testContract({account: 'teamgreymass'}))
+                const c4 = new testContract({
+                    client,
+                    account: Name.from('teamgreymass'),
                 })
-            }
-        } catch (e) {
-            console.log(e)
+                assertRewardsContract(c4)
+
+                const c5 = new testContract({
+                    client,
+                    abi: testNamespace.abi,
+                    account: 'teamgreymass',
+                })
+                assertRewardsContract(c5)
+
+                assert.throws(() => new testContract({abi: testNamespace.abi}))
+                assert.throws(
+                    () =>
+                        new testContract({
+                            abi: testNamespace.abi,
+                            account: 'teamgreymass',
+                        })
+                )
+                assert.throws(() => new testContract({account: 'teamgreymass'}))
+            })
         }
     })
     teardown(() => {

22d5f7b37b5b0ed5b8cbbe4428e263c19ecd128e
Daniel Fugere <danielfugere28@gmail.com>
2023-08-28 17:27:50 -0700

    Removed codegen logic (#55)
    * chore: removed codegen logic
    
    * Removed a few more things
    
    ---------
    
    Co-authored-by: Aaron Cox <aaron@greymass.com>

--- test/tests/helpers/generic.ts
+++ test/tests/helpers/generic.ts
@@ -1,8 +1,7 @@
 import {Action, Name} from '@wharfkit/antelope'
 import {assert} from 'chai'
 
-import {ActionDataType, Contract} from 'src/contract'
-import {Table} from 'src/index-module'
+import {ActionDataType, Contract, Table} from '@wharfkit/contract'
 
 // Mocks data for the first action defined in the contract for testing purposes
 export function getMockParams(contract: Contract): ActionDataType {

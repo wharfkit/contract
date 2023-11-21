8871b50ad3928e2960519d8c702c19e8ef1f07f1
Aaron Cox <aaron@greymass.com>
2023-11-20 17:16:49 -0800

    Changing default RowType

--- src/contract.ts
+++ src/contract.ts
@@ -72,7 +72,7 @@ export class Contract {
         return this.tableNames.includes(String(name))
     }
 
-    public table<RowType = any>(name: NameType, scope?: NameType, rowType?): Table<RowType> {
+    public table<RowType>(name: NameType, scope?: NameType, rowType?): Table<RowType | any> {
         if (!this.hasTable(name)) {
             throw new Error(`Contract (${this.account}) does not have a table named (${name})`)
         }
